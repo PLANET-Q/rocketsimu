19cd80e2a510af493f7535d852328f7595d9dda3
yamamotsu <motsulab@gmail.com>
2019-04-07 13:17:26 +0900

    bug fixed

--- test/test_lpf.py
+++ test/test_lpf.py
@@ -26,7 +26,7 @@ class TestLPF(unittest.TestCase):
         mask_10 = np.round(spectrum_10[(abs(freq_10) >= 10.0)], 7) == 0.0
 
         spectrum_8 = fft(lpf_8)
-        freq_8 = fftfreq(len(spectrum_10), d=dt)
+        freq_8 = fftfreq(len(spectrum_8), d=dt)
         mask_8 = np.round(spectrum_8[(abs(freq_8) >= 8.0)], 7) == 0.0
         self.assertEqual(mask_10.all(), True)
         self.assertEqual(mask_8.all(), True)
